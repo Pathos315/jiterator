dc03670b38ba17688f2545724542316c6aa27f7b
John Fallot <john.fallot@gmail.com>
2023-10-11 11:08:36 -0400

    Create cli.py
    Idea for a helper utility CLI to get pypy installed

---
+++ jiterator/jit/cli.py
@@ -0,0 +1,13 @@
+# This is a helper utility/CLI to get everything set up on first use. 
+# Once it's installed, the user should be able to create jiterators with ease.
+# N.B. This could be a shell file, perhaps? Writing this as commented pseudo-code for now.
+
+# import argparse
+
+# set up cli utility to install pypy if it's not present... not sure how to check for this.
+
+# subprocess("hg clone https://foss.heptapod.net/pypy/pypy pypy")
+# subprocess("python -mpip install cffi")
+
+
+# Possible pypy/module/pypyjit/moduledef.py
