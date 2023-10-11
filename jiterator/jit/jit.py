a0516160ccc8f2cddbd433e0581f93dac8369ca3
John Fallot <john.fallot@gmail.com>
2023-10-11 11:17:29 -0400

    Update jit.py
    Added a possible, alternative implementation

--- jiterator/jit/jit.py
+++ jiterator/jit/jit.py
@@ -1,6 +1,6 @@
 """This is where the class for the context manager is."""
-from collections.abc import Iterator
-
+from collections.abc import Iterator, Any
+from contextlib import contextmanager
 
 class JitContextManager:
     """The class for the context manager."""
@@ -15,3 +15,16 @@ class JitContextManager:
     def __exit__(self, exception_type, execution_value, execution_traceback):
         """Called when the context manager's usage is done."""
         print(f"exited with type: {exception_type}, value: {execution_value}, and tb: {execution_traceback}")
+
+### Possible alternative implementation below
+@contextmanager
+def jiterator(iterator: Iterator, operation: Callable[[Iterator],Any]) -> :
+    """Sets a destination for exported files."""
+    iterator = iterator
+    try:
+        subprocess.run("pypy --jit on")
+        result = operation(iterator)
+        yield # possibly yield result?
+    finally:
+        # print(f"exited with type: {exception_type}, value: {execution_value}, and tb: {execution_traceback}")
+        subprocess.run("pypy --jit off")
